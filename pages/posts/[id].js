8ac7fd48d4cfebcd30559798311f8c30acfbc8b8
takoyaki893 <coc4538@gmail.com>
2024-04-24 21:45:37 +0900

    fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import Date from '../../components/date';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
